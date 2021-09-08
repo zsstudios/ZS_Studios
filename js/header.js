47012c5ed5044e835382c499e79a7c5ef81d473b
ZIP <rimocon1222@outlook.jp>
2021-09-08 16:50:10 +0900

    JavaScriptでヘッダーを共通化するテストその2

--- js/header.js
+++ js/header.js
@@ -1,8 +1,11 @@
-function header(){
+function writeHeader(rootDir){
     $.ajax({
-        url: "../header.html",
-        cache: false,
+        url: rootDir + "header.html", //パスはcommon.jsが読み込まれたHTMLファイルが基準
+        cache: false, //キャッシュを利用するか（お好みで）
+        async: false, //非同期で読み込むか（お好みで）
         success: function(html){
+
+            html = html.replace(/\{\$root\}/g, rootDir);
             document.write(html);
         }
     });
